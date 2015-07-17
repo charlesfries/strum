928488d2b487a4f7bcbf36cae4aa6e9ad46cbae6
Charles Fries <charliefries@icloud.com>
2015-07-17 00:56:18 -0700

    Deep swiping integration (beta)

--- Application/Drum 2/Main.swift
+++ Application/Drum 2/Main.swift
@@ -9,6 +9,7 @@
 import UIKit
 import MediaPlayer
 import iAd
+import AudioToolbox
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -26,6 +27,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 class Main: UIViewController {
     
+    @IBOutlet var pageControl: UIPageControl!
+    var page = 5
+    
     // MARK: Config
     
     var refreshRate = 0.005
@@ -35,7 +39,7 @@ class Main: UIViewController {
     
     @IBOutlet weak var progress: UIView!
     @IBOutlet weak var label: UILabel!
-    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var button: UILabel!
     
     // MARK: Globals
     
@@ -50,17 +54,22 @@ class Main: UIViewController {
     
     @IBAction func left() {
         
-        
-        modifier = modifier + 10
-        
-        print("Left \(modifier)")
+        if !startToggle {
+            if page != 10 {
+                page++
+            }
+            pageControl.currentPage = page
+        }
     }
     
     @IBAction func right() {
         
-        modifier = modifier - 10
-        
-        print("Right \(modifier)")
+        if !startToggle {
+            if page != 0 {
+                page--
+            }
+            pageControl.currentPage = page
+        }
     }
     
     
@@ -93,26 +102,114 @@ class Main: UIViewController {
     }
     
     func update() {
-        let seconds = NSCalendar.currentCalendar().components([.Hour, .Minute, .Second], fromDate: NSDate()).second
+        var seconds = NSCalendar.currentCalendar().components([.Hour, .Minute, .Second], fromDate: NSDate()).second
+        
+        print("Real Seconds: \(seconds)")
+        
         
         // Change background color
-        changeColor(true)
+        //changeColor(true)
         
-        let progress = UInt8(Float(seconds % 10))
+        var progress = UInt8(Float(seconds % 10))
         
         // Update interface measures
+        
+        
+        
+        
+        
+        if page == 0 {
+            progress = 50 + progress
+            label.text = "-\(progress)s"
+            seconds = seconds + 10
+            
+        } else if page == 1 {
+            progress = 40 + progress
+            label.text = "-\(progress)s"
+            seconds = seconds + 20
+            
+        } else if page == 2 {
+            progress = 30 + progress
+            label.text = "-\(progress)s"
+            seconds = seconds + 30
+            
+        } else if page == 3 {
+            progress = 20 + progress
+            label.text = "-\(progress)s"
+            seconds = seconds + 40
+            
+        } else if page == 4 {
+            progress = 10 + progress
+            label.text = "-\(progress)s"
+            seconds = seconds + 50
+            
+        } else if page == 5 {
+            progress = 0 + progress
+            label.text = "\(10 - progress)s"
+            seconds = seconds + 0
+            
+        } else if page == 6 {
+            progress = 10 + progress
+            label.text = "\(30 - progress)s"
+            seconds = seconds + 10
+            
+        } else if page == 7 {
+            progress = 20 + progress
+            label.text = "\(50 - progress)s"
+            seconds = seconds + 20
+            
+        } else if page == 8 {
+            progress = 30 + progress
+            label.text = "\(70 - progress)s"
+            seconds = seconds + 30
+            
+        } else if page == 9 {
+            progress = 40 + progress
+            label.text = "\(90 - progress)s"
+            seconds = seconds + 40
+            
+        } else if page == 10 {
+            progress = 50 + progress
+            label.text = "\(110 - progress)s"
+            seconds = seconds + 50
+            
+        }
+        
+        var color = UIColor()
+        
+        if seconds >= 0 && seconds < 10 { // 0 to 9
+            color = UIColor(red:1, green:0.4, blue:0.4, alpha:1)
+        } else if seconds >= 10 && seconds < 20 { // 10 to 19
+            color = UIColor(red:1, green:0.8, blue:0.43, alpha:1)
+        } else if seconds >= 20 && seconds < 30 { // 20 to 29
+            color = UIColor(red:0.78, green:0.77, blue:0.35, alpha:1)
+        } else if seconds >= 30 && seconds < 40 { // 30 to 39
+            color = UIColor(red:0.24, green:0.79, blue:0.42, alpha:1)
+        } else if seconds >= 40 && seconds < 50 { // 40 to 49
+            color = UIColor(red:0.09, green:0.51, blue:0.98, alpha:1)
+        } else if seconds >= 50 && seconds < 60 { // 50 to 59
+            color = UIColor(red:0.8, green:0.42, blue:0.99, alpha:1)
+        } else {
+            color = UIColor.blackColor()
+        }
+        
+        view.backgroundColor = color
+        
+        
+        
+        
+        
         circle.progress = Double(progress) / 10
-        label.text = "\(10 - progress)s"
         
         // Shows Done message after syncing
         if hold > 0 {
             hold--
-            button.setTitle("Done!", forState: .Normal)
+            button.text = "Done!"
         } else {
             if !startToggle {
-                button.setTitle("Tap to Sync", forState: .Normal)
+                button.text = "Tap to Sync"
             } else {
-                button.setTitle("Cancel", forState: .Normal)
+                button.text = "Cancel"
             }
         }
         
@@ -166,16 +263,20 @@ class Main: UIViewController {
     func change() {
         if startToggle {
             startToggle = false
-            button.setTitle("Tap to Sync", forState: .Normal)
+            button.text = "Tap to Sync"
+            pageControl.hidden = false
         } else {
             startToggle = true
-            button.setTitle("Cancel", forState: .Normal)
+            button.text = "Cancel"
+            pageControl.hidden = true
         }
     }
     
     // MARK: Actions
     
     @IBAction func start() {
+        
+        AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
         
         let seconds = NSCalendar.currentCalendar().components([.Hour, .Minute, .Second], fromDate: NSDate()).second
         
